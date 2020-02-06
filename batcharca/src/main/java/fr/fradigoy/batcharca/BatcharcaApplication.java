2988668c25730e8b8d383b0b7281ecee8b237571
fredostar <dev@frederic-radigoy.fr>
2020-02-06 22:16:34 +0100

    commit creation du projet

---
+++ batcharca/src/main/java/fr/fradigoy/batcharca/BatcharcaApplication.java
@@ -0,0 +1,13 @@
+package fr.fradigoy.batcharca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatcharcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatcharcaApplication.class, args);
+	}
+
+}
---
+++ batcharca/src/test/java/fr/fradigoy/batcharca/BatcharcaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fradigoy.batcharca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatcharcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
